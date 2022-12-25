ae0844b4fdbce3af1b6711fcf3b033f4019938e7
YasirNaeem272 <104255987+YasirNaeem272@users.noreply.github.com>
2022-12-25 20:48:00 +0500

    TwoFactorAuthenticationInAspDotNetMvc

---
+++ TaskProject/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        //TestEntities _db = new TestEntities();
+        //[HttpPost]
+        //[Authorize]
+        //public ActionResult Login(UserTest userTest)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var check = _db.UserTests.Where(x => x.UserName == userTest.UserName && x.UserPaswd == userTest.UserPaswd).FirstOrDefault();
+        //        if (check != null)
+        //        {
+        //            return RedirectToAction("Homepage", "User");
+        //        }
+        //        else
+        //        {
+        //            RedirectToAction("Login", "User");
+        //        }
+
+        //    }
+        //    return View();
+        //}
+        //[HttpGet]
+        //public ActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult Homepage()
+        //{
+        //    return View();
+        //}
+    }
+}
