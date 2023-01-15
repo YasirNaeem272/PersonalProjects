f8407c49ad561f29ac887654684070c118ed4019
YasirNaeem272 <104255987+YasirNaeem272@users.noreply.github.com>
2023-01-15 20:49:16 +0500

    SendEmail
    Send Email

---
+++ SendEmail/Controllers/SendEmailController.cs
@@ -0,0 +1,69 @@
+﻿using SendEmail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SendEmail.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        // GET: SendEmail
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(Email model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("yasiralam272@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("yasiralam272@gmail.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "yasiralam272@gmail.com",  // replace with valid value
+                        Password = "pakistan345"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
+    }
+}
